d6389ef4d63714b05e7ead522284082073ea149b
John Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-15 14:42:44 -0600

    feat: finish a few ControlBlockBase functions & add skeleton for ControlBlock & SharedPtr

--- src/SharedPtr.h
+++ src/SharedPtr.h
@@ -3,10 +3,14 @@
 
 class ControlBlockBase {
 public:
-    ControlBlockBase(); // TODO: implement the default constructor.
+    // default constructor
+    ControlBlockBase(): m_refCount{0} {}
 
     // dtor is virtual, so that we can call derived class's dtor from a ptr to this base class.
-    virtual ~ControlBlockBase(); // TODO: implement the destructor.
+    // TODO: implement the destructor.
+    virtual ~ControlBlockBase() {
+        
+    } 
 
     // pure virtual function; must be overriden by derived classes
     virtual void* managedAddress() = 0;
@@ -17,21 +21,65 @@ public:
 
     long increment()
     {
-        // TODO: increment refcount by 1 and return result.
+        // increment refcount by 1 and return result.
+        return ++m_refCount;
     }
 
     long decrement()
     {
-        // TODO: decrement refcount by 1 and return result.
+        // decrement refcount by 1 and return result.
+        return --m_refCount;
     }
 
     long refCount() const
     {
-        // TODO: just return the refcount.
+        // just return the refcount.
+        return m_refCount;
     }
 
 private:
-    // TODO: add field(s) which both control block types need to have
+    // add field(s) which both control block types need to have
+    // 1. reference count
+    // NOTE: I am assuming just reference count because there could be 2 versions -> 1 storing ptr, 1 embedding the data directly
+    int m_refCount;
 };
+
+
+class ControlBlock : public ControlBlockBase {
+public:
+    void *managedAddress() {
+
+    }
+private:
+
+};
+
+template <typename T>
+class SharedPtr {
+public:
+    // constructors
+    // Case 1: no arguments
+    SharedPtr(): m_controlBlock{nullptr}, m_storedPtr{nullptr} {};
+
+    // Case 2: pointer given
+    SharedPtr(T *ptr): m_controlBlock{new ControlBlock()}, m_storedPtr{ptr} {
+        m_controlBlock->increment();
+    }
+
+    // destructor
+    ~SharedPtr() {
+        m_controlBlock->decrement();
+    }
+
+    // copy semantics
+//    SharedPtr(const SharedPtr<T> &sharedPtr) : m_
+
+
+private:
+    // our two raw pointers: 1. control block, 2. stored pointer
+    ControlBlock *m_controlBlock;
+    T *m_storedPtr;
+};
+
 
 #endif
